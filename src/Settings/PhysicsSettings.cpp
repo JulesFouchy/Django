6393b3c26c0218f0fc846c7d4a1c5a9f3ac37f38
Jules Fouchy <jules.fouchy@ntymail.com>
2020-01-30 21:55:35 +0100

    Rename dampening as damping

--- src/Settings/PhysicsSettings.cpp
+++ src/Settings/PhysicsSettings.cpp
@@ -5,7 +5,7 @@
 #include <imgui/imgui.h>
 
 PhysicsSettings::PhysicsSettings(ShaderPipeline& physicsShader, float raideur, float dampening)
-	: m_physicsShader(physicsShader), m_raideur(raideur), m_dampening(dampening)
+	: m_physicsShader(physicsShader), m_raideur(raideur), m_damping(dampening)
 {}
 
 void PhysicsSettings::setUniforms() {
@@ -21,13 +21,17 @@ void PhysicsSettings::setRaideurInShader() {
 
 void PhysicsSettings::setDampeningInShader() {
 	m_physicsShader.bind();
-	m_physicsShader.setUniform1f("u_Dampening", m_dampening);
+	m_physicsShader.setUniform1f("u_Damping", m_damping);
 	m_physicsShader.unbind();
 }
 
 void PhysicsSettings::ImGui_Parameters() {
 	if (ImGui::SliderFloat("Pull force", &m_raideur, 0.0f, 20.0f))
 		setRaideurInShader();
-	if (ImGui::SliderFloat("Dampening", &m_dampening, 0.0f, 20.0f))
+	if (ImGui::SliderFloat("Damping", &m_damping, 0.0f, 20.0f))
 		setDampeningInShader();
+	if (ImGui::Button("Perfect")) {
+		m_raideur = m_damping * m_damping / 4.0f;
+		setRaideurInShader();
+	}
 }
