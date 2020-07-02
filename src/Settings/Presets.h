1eb0b2f2681c0e202c8c676133176509033abd5f
Jules Fouchy <jules.fouchy@ntymail.com>
2020-07-02 20:12:09 +0200

    Merge preset name and value vectors into one to be able to sort by name, and implement the sort !

--- src/Settings/Presets.h
+++ src/Settings/Presets.h
@@ -6,6 +6,12 @@ namespace fs = std::filesystem;
 #include <fstream>
 
 template <typename T>
+struct Preset {
+	std::string name;
+	T values;
+};
+
+template <typename T>
 class Presets {
 public:
 	Presets(const char* fileExtension, const std::string& settingsFolderPath)
@@ -19,10 +25,10 @@ public:
 	bool ImGui(T* settingValues) {
 		bool b;
 		if (b = ImGui::BeginCombo("Presets", m_currentPresetName, 0)) {
-			for (size_t i = 0; i < m_presetsNames.size(); i++) {
-				if (ImGui::Selectable(m_presetsNames[i].c_str(), false)) {
-					m_currentPresetName = m_presetsNames[i].c_str();
-					*settingValues = m_presets[i];
+			for (size_t i = 0; i < m_presets.size(); i++) {
+				if (ImGui::Selectable(m_presets[i].name.c_str(), false)) {
+					m_currentPresetName = m_presets[i].name.c_str();
+					*settingValues = m_presets[i].values;
 				}
 			}
 			ImGui::EndCombo();
@@ -45,27 +51,44 @@ public:
 	}
 
 private:
+	void sort() {
+		// Case insensitive alphabetical sort
+		std::sort(m_presets.begin(), m_presets.end(), [](const Preset<T>& l, const Preset<T>& r) {
+			return std::equal(
+				l.name.begin(), l.name.end(),
+				r.name.begin(), r.name.end(),
+				[](char a, char b) {
+					return tolower(a) == tolower(b);
+			});;
+		});
+		// Put Default first
+		for (size_t i = 0; i < m_presets.size(); ++i) {
+			if (!m_presets[i].name.compare("Default")) {
+				Preset<T> prst = m_presets[i];
+				std::copy_backward(m_presets.begin(), i + m_presets.begin(), i + 1 + m_presets.begin());
+				m_presets[0] = prst;
+				break;
+			}
+		}
+	}
+
 	void loadPresetsFrom(const std::string& folderPath) {
 		for (const auto& file : fs::directory_iterator(folderPath)) {
 			if (!file.path().filename().replace_extension("").extension().string().compare(m_fileExtension)) {
 				spdlog::warn(file.path().string());
-				m_presetsNames.push_back(file.path().filename().replace_extension("").replace_extension("").string());
-				T preset;
+				std::string name = file.path().filename().replace_extension("").replace_extension("").string();
+				T values;
 				std::ifstream is(file.path());
 				{
 					cereal::JSONInputArchive archive(is);
 					archive(
-						preset
+						values
 					);
 				}
-				m_presets.push_back(preset);
-				// Default is always put first in the list
-				if (!m_presetsNames.back().compare("Default")) {
-					std::swap(m_presetsNames.back(), m_presetsNames[0]);
-					std::swap(m_presets.back(), m_presets[0]);
-				}
+				m_presets.push_back({ name, values });
 			}
 		}
+		sort();
 	}
 
 	void savePresetTo(T& settingValues, const std::string& folderPath) {
@@ -77,14 +100,13 @@ private:
 			);
 		}
 		// Add it to current list
-		m_presets.push_back(settingValues);
-		m_presetsNames.push_back(m_savePresetAs);
+		m_presets.push_back({ m_savePresetAs, settingValues });
+		sort();
 	}
 
 private:
 	const std::string m_fileExtension;
-	std::vector<T> m_presets;
-	std::vector<std::string> m_presetsNames;
+	std::vector<Preset<T>> m_presets;
 	const char* m_currentPresetName;
 	std::string m_savePresetAs;
 };
