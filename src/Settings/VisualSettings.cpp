b234c490a17f168a545c24fdc22d269271a28077
Jules Fouchy <jules.fouchy@ntymail.com>
2020-07-03 00:10:58 +0200

    Add Presets to VisualSettings

--- src/Settings/VisualSettings.cpp
+++ src/Settings/VisualSettings.cpp
@@ -1,25 +1,38 @@
 #include "VisualSettings.h"
 
+VisualSettings::VisualSettings()
+	: m_presets("djgStyle")
+{}
+
 void VisualSettings::apply() {
-	if (m_bAlphaTrail)
+	if (m_values.bAlphaTrail)
 		EnableAlphaTrail();
 	else
 		DisableAlphaTrail();
 }
 
 void VisualSettings::ImGui() {
+	bool b = false;
 	// Alpha trail
 		// toggle
-	if (ImGui::Checkbox("Alpha Trail", &m_bAlphaTrail)) {
-		if (m_bAlphaTrail)
+	if (ImGui::Checkbox("Alpha Trail", &m_values.bAlphaTrail)) {
+		b = true;
+		if (m_values.bAlphaTrail)
 			EnableAlphaTrail();
 		else
 			DisableAlphaTrail();
 	}
 		// strength
-	ImGui::SliderFloat("Trail Decay", &m_alphaTrailDecay, 0.0f, 60.0f);
+	if (ImGui::SliderFloat("Trail Decay", &m_values.alphaTrailDecay, 0.0f, 60.0f))
+		b = true;
 	// Background Color
-	ImGui::ColorEdit3("Background Color", (float*)&m_bgColor[0]);
+	if (ImGui::ColorEdit3("Background Color", (float*)&m_values.bgColor[0]))
+		b = true;
+	if (m_presets.ImGui(&m_values)) {
+		apply();
+	}
+	if (b)
+		m_presets.setToPlaceholderSetting();
 }
 
 void VisualSettings::EnableAlphaTrail() {
--- src/Settings/VisualSettings.h
+++ src/Settings/VisualSettings.h
@@ -1,26 +1,11 @@
 #pragma once
 
-class VisualSettings {
-public:
-	VisualSettings() = default;
-	~VisualSettings() = default;
+#include "Presets.h"
 
-	void apply();
-	void ImGui();
-
-	// Getters
-	inline bool isAlphaTrailEnabled() { return m_bAlphaTrail; }
-	inline float alphaTrailDecay() { return m_alphaTrailDecay; }
-	inline const glm::vec3& backgroundColor() { return m_bgColor; }
-
-private:
-	static void  EnableAlphaTrail();
-	static void DisableAlphaTrail();
-
-private:
-	bool m_bAlphaTrail = true;
-	float m_alphaTrailDecay = 20.0f;
-	glm::vec3 m_bgColor = glm::vec3(0.0f, 0.0f, 0.0f);
+struct VisualSettingsValues {
+	bool bAlphaTrail = true;
+	float alphaTrailDecay = 20.0f;
+	glm::vec3 bgColor = glm::vec3(0.0f, 0.0f, 0.0f);
 
 private:
 	//Serialization
@@ -29,11 +14,45 @@ private:
 	void serialize(Archive& archive)
 	{
 		archive(
-			CEREAL_NVP(m_bAlphaTrail),
-			CEREAL_NVP(m_alphaTrailDecay),
-			CEREAL_NVP(m_bgColor.r),
-			CEREAL_NVP(m_bgColor.g),
-			CEREAL_NVP(m_bgColor.b)
+			CEREAL_NVP(bAlphaTrail),
+			CEREAL_NVP(alphaTrailDecay),
+			CEREAL_NVP(bgColor.r),
+			CEREAL_NVP(bgColor.g),
+			CEREAL_NVP(bgColor.b)
+		);
+	}
+};
+
+class VisualSettings {
+public:
+	VisualSettings();
+	~VisualSettings() = default;
+
+	void apply();
+	void ImGui();
+
+	// Getters
+	inline bool isAlphaTrailEnabled() { return m_values.bAlphaTrail; }
+	inline float alphaTrailDecay() { return m_values.alphaTrailDecay; }
+	inline const glm::vec3& backgroundColor() { return m_values.bgColor; }
+
+private:
+	static void  EnableAlphaTrail();
+	static void DisableAlphaTrail();
+
+private:
+	VisualSettingsValues m_values;
+	Presets<VisualSettingsValues> m_presets;
+
+private:
+	// Serialization
+	friend class cereal::access;
+	template <class Archive>
+	void serialize(Archive& archive)
+	{
+		archive(
+			CEREAL_NVP(m_values),
+			CEREAL_NVP(m_presets)
 		);
 	}
 };
