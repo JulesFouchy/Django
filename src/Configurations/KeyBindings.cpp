82cd4cd78542221484ad76f013e5165eee2aa697
Jules Fouchy <jules.fouchy@ntymail.com>
2020-08-02 09:20:43 +0100

    Slightly better key <-> action bindings (take ActionType into account)

--- src/Configurations/KeyBindings.cpp
+++ src/Configurations/KeyBindings.cpp
@@ -8,6 +8,60 @@ Action* KeyBindings::getAction(SDL_Scancode scancode) {
 	return nullptr;
 }
 void KeyBindings::addAction(Action action) {
-	if (nextAvailableKeyIdx<26)
-	m_map[keys[nextAvailableKeyIdx++]] = action;
+	SDL_Scancode scancode;
+	switch (action.type) {
+	case ActionType::TEXT:
+		m_map[SDL_SCANCODE_RETURN] = action;
+		break;
+	case ActionType::REROLL_RANDOM:
+		m_map[SDL_SCANCODE_SPACE] = action;
+		break;
+	case ActionType::LAYOUT:
+		scancode = findFirstFromLeft(secondRow);
+		if (scancode != SDL_SCANCODE_UNKNOWN)
+			m_map[scancode] = action;
+		break;
+	case ActionType::SHAPE:
+	case ActionType::SVG_SHAPE:
+		scancode = findFirstFromLeft(firstRow);
+		if (scancode == SDL_SCANCODE_UNKNOWN) {
+			scancode = findFirstFromRight(secondRow);
+		}
+		if (scancode != SDL_SCANCODE_UNKNOWN) {
+			m_map[scancode] = action;
+		}
+		break;
+	case ActionType::STANDALONE:
+		scancode = findFirstFromLeft(thirdRow);
+		if (scancode == SDL_SCANCODE_UNKNOWN) {
+			scancode = findFirstFromRight(secondRow);
+		}
+		if (scancode != SDL_SCANCODE_UNKNOWN) {
+			m_map[scancode] = action;
+		}
+		break;
+	default:
+		assert(false && "Unsupported ActionType !");
+		break;
+	}
+}
+
+bool KeyBindings::isKeyAvailable(SDL_Scancode scancode) {
+	return m_map.find(scancode) == m_map.end();
+}
+
+SDL_Scancode KeyBindings::findFirstFromLeft(std::vector<SDL_Scancode> row) {
+	for (size_t i = 0; i < row.size(); ++i) {
+		if (isKeyAvailable(row[i]))
+			return row[i];
+	}
+	return SDL_SCANCODE_UNKNOWN;
+}
+
+SDL_Scancode KeyBindings::findFirstFromRight(std::vector<SDL_Scancode> row) {
+	for (size_t i = row.size() - 1; i >= 0; --i) {
+		if (isKeyAvailable(row[i]))
+			return row[i];
+	}
+	return SDL_SCANCODE_UNKNOWN;
 }
--- src/Configurations/KeyBindings.h
+++ src/Configurations/KeyBindings.h
@@ -30,9 +30,14 @@ public:
 	void addAction(Action action);
 
 private:
+	bool isKeyAvailable(SDL_Scancode scancode);
+	SDL_Scancode findFirstFromLeft(std::vector<SDL_Scancode> row);
+	SDL_Scancode findFirstFromRight(std::vector<SDL_Scancode> row);
+
+private:
 	std::unordered_map<int, Action> m_map;
 	size_t nextAvailableKeyIdx = 0;
-	std::vector<SDL_Scancode> keys = {
+	std::vector<SDL_Scancode> firstRow = {
 		SDL_SCANCODE_Q,
 		SDL_SCANCODE_W,
 		SDL_SCANCODE_E,
@@ -43,6 +48,11 @@ private:
 		SDL_SCANCODE_I,
 		SDL_SCANCODE_O,
 		SDL_SCANCODE_P,
+		SDL_SCANCODE_LEFTBRACKET,
+		SDL_SCANCODE_RIGHTBRACKET,
+		SDL_SCANCODE_BACKSLASH,
+	};
+	std::vector<SDL_Scancode> secondRow = {
 		SDL_SCANCODE_A,
 		SDL_SCANCODE_S,
 		SDL_SCANCODE_D,
@@ -52,12 +62,19 @@ private:
 		SDL_SCANCODE_J,
 		SDL_SCANCODE_K,
 		SDL_SCANCODE_L,
+		SDL_SCANCODE_SEMICOLON,
+		SDL_SCANCODE_APOSTROPHE,
+	};
+	std::vector<SDL_Scancode> thirdRow = {
 		SDL_SCANCODE_Z,
 		SDL_SCANCODE_X,
 		SDL_SCANCODE_C,
 		SDL_SCANCODE_V,
 		SDL_SCANCODE_B,
 		SDL_SCANCODE_N,
-		SDL_SCANCODE_M
+		SDL_SCANCODE_M,
+		SDL_SCANCODE_COMMA,
+		SDL_SCANCODE_PERIOD,
+		SDL_SCANCODE_SLASH,
 	};
 };
