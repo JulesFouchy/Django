9e4f487c11144d69b7bc996e87666cbaa3243947
Jules Fouchy <jules.fouchy@ntymail.com>
2020-08-30 12:55:21 +0200

    Bug fix with ConfigParams onKeyPressed

--- src/Configurations/ConfigParams.cpp
+++ src/Configurations/ConfigParams.cpp
@@ -10,63 +10,79 @@ bool ConfigParams::onKeyPressed(SDL_Scancode scancode, StateModifier& stateModif
     case SDL_SCANCODE_KP_MINUS:
         count--;
         b = true;
+        break;
     case SDL_SCANCODE_KP_PLUS:
         count++;
         b = true;
+        break;
     case SDL_SCANCODE_KP_0:
     case SDL_SCANCODE_0:
         count = 0;
         b = true;
+        break;
     case SDL_SCANCODE_KP_1:
     case SDL_SCANCODE_1:
         count = 1;
         b = true;
+        break;
     case SDL_SCANCODE_KP_2:
     case SDL_SCANCODE_2:
         count = 2;
         b = true;
+        break;
     case SDL_SCANCODE_KP_3:
     case SDL_SCANCODE_3:
         count = 4;
         b = true;
+        break;
     case SDL_SCANCODE_KP_4:
     case SDL_SCANCODE_4:
         count = 8;
         b = true;
+        break;
     case SDL_SCANCODE_KP_5:
     case SDL_SCANCODE_5:
         count = 16;
         b = true;
+        break;
     case SDL_SCANCODE_KP_6: 
     case SDL_SCANCODE_6:
         count = 32;
         b = true;
+        break;
     case SDL_SCANCODE_KP_7:
     case SDL_SCANCODE_7:
         count = 64;
         b = true;
+        break;
     case SDL_SCANCODE_KP_8:
     case SDL_SCANCODE_8:
         count = 128;
         b = true;
+        break;
     case SDL_SCANCODE_KP_9:
     case SDL_SCANCODE_9:
         count = 256;
         b = true;
+        break;
     // LR
     case SDL_SCANCODE_LEFT:
         intLR = std::max(intLR-1, 1);
         b = true;
+        break;
     case SDL_SCANCODE_RIGHT:
         intLR++;
         b = true;
+        break;
     // UD
     case SDL_SCANCODE_DOWN:
         intUD--;
         return true;
+        break;
     case SDL_SCANCODE_UP:
         intUD++;
         b = true;
+        break;
     }
     if (b) {
         stateModifier.apply();
