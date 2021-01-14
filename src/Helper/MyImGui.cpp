a162bba61f883a185f4e794ab309aec10972a97c
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-14 22:50:56 +0100

    MyImGui : add BeginPopupContextMenuFromButton

--- src/Helper/MyImGui.cpp
+++ src/Helper/MyImGui.cpp
@@ -183,3 +183,15 @@ void MyImGui::ImageFramed(unsigned int texID, const ImVec2& size, int frameThick
 bool MyImGui::InputUInt(const char* label, unsigned int* value_p) {
 	return ImGui::InputScalar(label, ImGuiDataType_U32, value_p, NULL, NULL, "%u");
 }
+
+bool MyImGui::BeginPopupContextMenuFromButton(const char* label, ImGuiPopupFlags popup_flags) {
+	using namespace ImGui;
+	ImGuiWindow* window = GImGui->CurrentWindow;
+	if (window->SkipItems)
+		return false;
+	ImGuiID id = label ? window->GetID(label) : window->DC.LastItemId; // If user hasn't passed an ID, we can use the LastItemID. Using LastItemID as a Popup ID won't conflict!
+	IM_ASSERT(id != 0);                                                  // You cannot pass a NULL str_id if the last item has no identifier (e.g. a Text() item)
+	if (ImGui::Button(label))
+		OpenPopupEx(id, popup_flags);
+	return BeginPopupEx(id, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoSavedSettings);
+}
